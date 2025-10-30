b91da4d817f87d799d6574ed46a8240771e612f6
ShrivastavaPrachi <142006316+ShrivastavaPrachi@users.noreply.github.com>
2025-10-30 20:25:38 +0000

    hvhj

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -1,1 +1,32 @@
 //Dates
+// let myDate =new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+//let myCreatedDate =new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString())
+
+// let myCreatedDate =new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate =new Date("01-28-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+// let mytimeStamp = Date.now()
+// console.log(mytimeStamp);
+
+//console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
+console.log(newDate);
+
